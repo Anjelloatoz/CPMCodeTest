41ff54aeeb6b4fa5c63a025f1e9de505a386504f
Anjelloatoz <anjelloatoz@googlemail.com>
2018-08-11 14:40:39 +0200

    Initial project commit

---
+++ MyApplication/app/src/main/java/anjelloatoz/com/codetestapp/MainActivity.java
@@ -0,0 +1,13 @@
+package anjelloatoz.com.codetestapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
